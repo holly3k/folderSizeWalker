76361528b31aa98f0bb04edf50976a5eb4ec5f10
holly3k <mai.lingjun@outlook.com>
2023-09-22 00:45:54 +0800

    Update folderSizeWalker.go

--- folderSizeWalker.go
+++ folderSizeWalker.go
@@ -1,9 +1,12 @@
+// go build -o folderSizeWalker.dll -buildmode=c-shared folderSizeWalker.go
 package main
 
+import "C"
 import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -11,24 +14,33 @@ import (
 )
 
 type File struct {
-	FullPath     string
-	Size         float64
-	SizeReadable string
-	Childs       []File
+	FullPath     string  `json:"path"`
+	Size         float64 `json:"size"`
+	SizeReadable string  `json:"readableSize"`
+	Childs       []File  `json:"childs"`
+	Type         string  `json:"type"`
 }
 
-func main() {
-	var dirPath string = "E:\\"
+//export startScanning
+func startScanning(s *C.char) {
+	var dirPath string = C.GoString(s)
+	startScanningWrapper(dirPath)
+}
+
+func startScanningWrapper(dirPath string) {
 	t := time.Now()
 	folderDetail := scanDir(dirPath)
 	fmt.Println(folderDetail.FullPath)
-	fmt.Println(folderDetail.Size)
+	fmt.Println(folderDetail.SizeReadable)
 	fmt.Println(len(folderDetail.Childs))
 	jsonFolderDetails, err := json.Marshal(folderDetail)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	} else {
+		if err := os.Remove("out.json"); err != nil {
+			log.Println(err)
+		}
 		err = ioutil.WriteFile("out.json", jsonFolderDetails, 0644)
 		if err != nil {
 			fmt.Println(err)
@@ -38,12 +50,18 @@ func main() {
 	fmt.Println("time spend:" + time.Since(t).String())
 }
 
+func main() {
+	var dirPath string = os.Args[1]
+	startScanningWrapper(dirPath)
+}
+
 func scanDir(path string) File {
 
 	dirAry, err := ioutil.ReadDir(path)
 	folderDetail := File{}
 	folderDetail.FullPath = path
 	folderDetail.Size = 0
+	folderDetail.Type = "FOLDER"
 	folderDetail.SizeReadable = "0kb"
 	folderDetail.Childs = []File{}
 	if err != nil {
@@ -56,7 +74,7 @@ func scanDir(path string) File {
 			folderDetail.Size += subFolder.Size
 		} else {
 			folderDetail.Size += float64(e.Size())
-			folderDetail.Childs = append(folderDetail.Childs, File{FullPath: filepath.Join(path, e.Name()), Size: float64(e.Size()), SizeReadable: formatSize(float64(e.Size()))})
+			folderDetail.Childs = append(folderDetail.Childs, File{Type: "FILE", Childs: []File{}, FullPath: filepath.Join(path, e.Name()), Size: float64(e.Size()), SizeReadable: formatSize(float64(e.Size()))})
 		}
 	}
 	folderDetail.SizeReadable = formatSize(folderDetail.Size)
@@ -65,26 +83,26 @@ func scanDir(path string) File {
 
 func formatSize(size float64) string {
 	if size < 1024 {
-		return strconv.FormatFloat(size, 'f', 5, 32) + "bit"
+		return strconv.FormatFloat(size, 'f', 2, 32) + "bit"
 	} else {
 		size = size / 1024
 		if size < 1024 {
-			return strconv.FormatFloat(size, 'f', 5, 32) + "k"
+			return strconv.FormatFloat(size, 'f', 2, 32) + "k"
 		} else {
 			size = size / 1024
 			if size < 1024 {
-				return strconv.FormatFloat(size, 'f', 5, 32) + "M"
+				return strconv.FormatFloat(size, 'f', 2, 32) + "M"
 			} else {
 				size = size / 1024
 				if size < 1024 {
-					return strconv.FormatFloat(size, 'f', 5, 32) + "G"
+					return strconv.FormatFloat(size, 'f', 2, 32) + "G"
 				} else {
 					size = size / 1024
 					if size < 1024 {
-						return strconv.FormatFloat(size, 'f', 5, 32) + "T"
+						return strconv.FormatFloat(size, 'f', 2, 32) + "T"
 					} else {
 						size = size / 1024
-						return strconv.FormatFloat(size, 'f', 5, 32) + "P"
+						return strconv.FormatFloat(size, 'f', 2, 32) + "P"
 					}
 				}
 			}
